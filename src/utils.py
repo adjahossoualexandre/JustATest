6e77d0f4d98de3868810d679855989c8a8b9e75b
alexandre <adjahossoualexandre@gmail.com>
2021-02-20 01:07:41 +0100

    Initial commit: add everything that was in envTest

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def num_to_word(x):
+
+    if x == 0:
+        return 'zero'
+    elif x == 1:
+        return 'un'
+    elif x == 2:
+        return 'deux'
+    else:
+        return 'flemme'
+
+
+
